26f7c9dec8ed2007c72906d84271285f2bb2ee33
ABizoman <othmanachki@icloud.com>
2025-04-30 12:30:24 +0100

    UI + webpage changes

---
+++ SampleRecorder/RecordingView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordingView.swift
+//  SampleRecorder
+//
+//  Created by Othman on 30/04/2025.
+//
+
+import SwiftUI
+
+struct RecordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordingView()
+}
---
+++ SampleRecorder/TP-7.swift
@@ -0,0 +1,18 @@
+//
+//  TP-7.swift
+//  SampleRecorder
+//
+//  Created by Othman on 30/04/2025.
+//
+
+import SwiftUI
+
+struct TP_7: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TP_7()
+}
